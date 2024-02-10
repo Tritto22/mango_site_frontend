005c789362cb4eeddb436545c0d7d6f50c73f643
Simone <tritto.simone@gmail.com>
2024-02-10 12:29:42 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/mango_site_frontend/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   outputDir: 'docs',
   pages: {
     index: {
