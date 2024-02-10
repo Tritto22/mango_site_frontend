c8feebf1459e297aad9dcf3e9ed0276f7bbeedf9
Simone <tritto.simone@gmail.com>
2024-02-10 12:12:21 +0100

    fix spa

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ Vue.use(Router);
 
 const router = new Router({
     mode: 'history',
+    base: 'https://tritto22.github.io/mango_site_frontend',
     routes: [
         // ****************************** PUBLIC PATH ***********************************
         {
